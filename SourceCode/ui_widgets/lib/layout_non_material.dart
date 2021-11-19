c5bbb89ace68f7f179e5fb9e89600bbb2e02b490
Neo Shi <shiwenxuan2003@gmail.com>
2021-11-19 19:29:16 +0800

    sourcecode

---
+++ SourceCode/ui_widgets/lib/layout_non_material.dart
@@ -0,0 +1,30 @@
+// This app doesn't use any Material widgets, such as Scaffold.
+// Normally, an app that doesn't use Scaffold has a black background
+// and the default text color is black. This app changes its background
+// to white and its text color to dark grey to mimic a material app.
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+// #docregion MyApp
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(color: Colors.white),
+      child: const Center(
+        child: Text(
+          'Hello World',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+            fontSize: 32,
+            color: Colors.black87,
+          ),
+        ),
+      ),
+    );
+  }
+}
